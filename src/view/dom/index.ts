343e777576b7edfd80b22cc32aea6b099ef3ebf3
bouzuya <m@bouzuya.net>
2016-08-16 13:04:08 +0900

    extract view/dom/

---
+++ src/view/dom/index.ts
@@ -0,0 +1,39 @@
+import xs from 'xstream';
+import { div, input, p, ul, li, VNode } from '@cycle/dom';
+import { Entry, State } from '../../type';
+
+const entryView = (entry: Entry): VNode => {
+  return div('.entry', [
+    div('.header', [entry.title]),
+    div('.body', [entry.body])
+  ]);
+};
+
+const entriesView = (entries: Entry[]): VNode => {
+  return div([
+    ul(
+      '.entry-list',
+      entries.map((entry) => {
+        return li(
+          '.entry-list-item', [
+            entryView(entry)
+          ]);
+      }))
+  ]);
+};
+
+const appView = (state: State): VNode => {
+  const { checked, entries } = state;
+  return div([
+    input({ attrs: { type: 'checkbox' } }), 'Toggle me',
+    p(checked ? 'ON' : 'off'),
+    entriesView(entries)
+  ]);
+};
+
+const view = (state$: xs<State>): xs<VNode> => {
+  const vnode$ = state$.map((state) => appView(state));
+  return vnode$;
+};
+
+export { view };
--- src/view/index.ts
+++ src/view/index.ts
@@ -1,47 +1,9 @@
 import xs from 'xstream';
-import { div, input, p, ul, li, VNode } from '@cycle/dom';
-import { Entry, State } from '../type';
+import { view as domView } from './dom';
+import { State } from '../type';
 
-// view for DOM driver
-
-const entryView = (entry: Entry): VNode => {
-  return div('.entry', [
-    div('.header', [entry.title]),
-    div('.body', [entry.body])
-  ]);
-};
-
-const entriesView = (entries: Entry[]): VNode => {
-  return div([
-    ul(
-      '.entry-list',
-      entries.map((entry) => {
-        return li(
-          '.entry-list-item', [
-            entryView(entry)
-          ]);
-      }))
-  ]);
-};
-
-const appView = (state: State): VNode => {
-  const { checked, entries } = state;
-  return div([
-    input({ attrs: { type: 'checkbox' } }), 'Toggle me',
-    p(checked ? 'ON' : 'off'),
-    entriesView(entries)
-  ]);
-};
-
-const render = (state$: xs<State>): xs<VNode> => {
-  const vnode$ = state$.map((state) => appView(state));
-  return vnode$;
-};
-
-// view for all drivers (Sinks)
 const view = (state$: xs<State>): { DOM: xs<any>; } => {
-  const vnode$ = render(state$);
-  const sinks = { DOM: vnode$ };
+  const sinks = { DOM: domView(state$) };
   return sinks;
 };
 
