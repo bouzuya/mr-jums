252b2a5e04d2b90440621d6fd88dcf0907b6dc14
bouzuya <m@bouzuya.net>
2016-08-16 18:57:23 +0900

    use selected-entry-id-in-list instead of selected-entry-id

--- src/view/dom/app.ts
+++ src/view/dom/app.ts
@@ -4,12 +4,12 @@ import { view as entryListView } from './entry-list';
 import { view as entryDetailView } from './entry-detail';
 
 const view = (state: State): VNode => {
-  const { checked, entries, selectedEntryId } = state;
+  const { checked, entries, selectedEntryIdInList, selectedEntryId } = state;
   return div([
     input({ attrs: { type: 'checkbox' } }), 'Toggle me',
     p(checked ? 'ON' : 'off'),
     div([
-      entryListView(entries, selectedEntryId),
+      entryListView(entries, selectedEntryIdInList),
       entryDetailView(entries, selectedEntryId),
       div('.next', ['J']),
       div('.prev', ['K'])
