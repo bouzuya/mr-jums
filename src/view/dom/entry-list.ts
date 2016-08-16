52e0274ac4d49fac191a721ae66fdded99bb9b75
bouzuya <m@bouzuya.net>
2016-08-16 17:09:12 +0900

    add entry max count

--- src/view/dom/entry-list.ts
+++ src/view/dom/entry-list.ts
@@ -3,10 +3,11 @@ import { Entry } from '../../type';
 import { view as entryView } from './entry';
 
 const view = (entries: Entry[]): VNode => {
+  const max = 10;
   return div([
     ul(
       '.entry-list',
-      entries.map((entry) => {
+      entries.filter((_, index) => index < max).map((entry) => {
         return li(
           '.entry-list-item.entry-id-' + entry.id, [
             entryView(entry)
