7e8d5306856ef83d18d9b9163b1813b40fd2e80f
bouzuya <m@bouzuya.net>
2016-08-16 17:36:49 +0900

    refactor intent

--- src/intent.ts
+++ src/intent.ts
@@ -2,12 +2,8 @@ import xs from 'xstream';
 import { Action } from './action';
 import { DOMSource } from '@cycle/dom';
 
-const intent = ({ DOM }: { DOM: DOMSource }): xs<Action> => {
-  const clickCheckbox$: xs<Event> = DOM.select('input').events('click');
+const select$ = ({ DOM }: { DOM: DOMSource }): xs<Action> => {
   const clickList$: xs<Event> = DOM.select('li').events('click');
-  const toggle$: xs<Action> = clickCheckbox$
-    .map((event) => (event.target as any).checked)
-    .map<Action>((checked) => ({ type: 'toggle', checked }));
   const select$: xs<Action> = clickList$
     .map((event) => {
       let target = event.target as Element;
@@ -25,8 +21,22 @@ const intent = ({ DOM }: { DOM: DOMSource }): xs<Action> => {
     })
     .filter((entryId) => typeof entryId !== 'undefined')
     .map<Action>((entryId: string) => ({ type: 'select', entryId }));
-  const action$: xs<Action> = xs
-    .merge(toggle$, select$);
+  return select$;
+};
+
+const toggle$ = ({ DOM }: { DOM: DOMSource }): xs<Action> => {
+  const clickCheckbox$: xs<Event> = DOM.select('input').events('click');
+  const toggle$: xs<Action> = clickCheckbox$
+    .map((event) => (event.target as any).checked)
+    .map<Action>((checked) => ({ type: 'toggle', checked }));
+  return toggle$;
+};
+
+const intent = ({ DOM }: { DOM: DOMSource }): xs<Action> => {
+  const action$: xs<Action> = xs.merge(
+    select$({ DOM }),
+    toggle$({ DOM })
+  );
   return action$;
 };
 
