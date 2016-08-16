ca000741534fca49f5cf12538b3db9b3ed5b8e3c
bouzuya <m@bouzuya.net>
2016-08-16 12:03:44 +0900

    add entry to model

--- src/model.ts
+++ src/model.ts
@@ -1,16 +1,33 @@
 import xs from 'xstream';
 import { Action, ActionType, ToggleAction } from './action';
 
+export interface Entry {
+  title: string;
+  body: string;
+}
+
+export interface State {
+  entries: Entry[];
+  checked: boolean;
+}
+
 const select = <T extends Action>(
   action$: xs<Action>, type: ActionType
 ): xs<T> => {
   return action$.filter((action) => action.type === type) as xs<T>;
 };
 
-const model = (action$: xs<Action>): xs<boolean> => {
-  const state$: xs<boolean> = select<ToggleAction>(action$, 'toggle')
+const model = (action$: xs<Action>): xs<State> => {
+  const checked$: xs<boolean> = select<ToggleAction>(action$, 'toggle')
     .map(({ checked }) => checked)
     .startWith(false);
+  const entries$: xs<Entry[]> = xs.of([
+    { title: 'My blog is dead', body: 'Good-bye, bbn-cycle!' },
+    { title: 'My first entry', body: 'Hello, bbn-cycle!' }
+  ]);
+  const state$: xs<State> = xs
+    .combine(checked$, entries$)
+    .map(([checked, entries]) => ({ checked, entries }));
   return state$;
 };
 
