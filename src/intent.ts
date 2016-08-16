7d8a67e440b5f50e713de6f3a6b3eed381a7957b
bouzuya <m@bouzuya.net>
2016-08-16 14:24:14 +0900

    add select-action to intent

--- src/intent.ts
+++ src/intent.ts
@@ -3,10 +3,30 @@ import { Action } from './action';
 import { DOMSource } from '@cycle/dom';
 
 const intent = ({ DOM }: { DOM: DOMSource }): xs<Action> => {
-  const click$: xs<Event> = DOM.select('input').events('click');
-  const action$: xs<Action> = click$
+  const clickCheckbox$: xs<Event> = DOM.select('input').events('click');
+  const clickList$: xs<Event> = DOM.select('li').events('click');
+  const toggle$: xs<Action> = clickCheckbox$
     .map((event) => (event.target as any).checked)
     .map<Action>((checked) => ({ type: 'toggle', checked }));
+  const select$: xs<Action> = clickList$
+    .map((event) => {
+      let target = event.target as Element;
+      while (target && target.tagName !== 'LI') {
+        target = target.parentElement;
+      }
+      const classList: string[] = Array.from(target.classList);
+      const entryId: string | undefined = classList
+        .map((c) => {
+          const m = c.match(/^entry-id-(.*)$/);
+          return m === null ? undefined : m[1];
+        })
+        .filter((i) => typeof i !== 'undefined')[0];
+      return entryId;
+    })
+    .filter((entryId) => typeof entryId !== 'undefined')
+    .map<Action>((entryId: string) => ({ type: 'select', entryId }));
+  const action$: xs<Action> = xs
+    .merge(toggle$, select$);
   return action$;
 };
 
