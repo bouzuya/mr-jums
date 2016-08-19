8031c63283dfef73bdb8c674a8d05cf4c730e337
bouzuya <m@bouzuya.net>
2016-08-20 08:52:41 +0900

    add is-selected css class

--- src/view/dom/entry-list.ts
+++ src/view/dom/entry-list.ts
@@ -5,13 +5,15 @@ import { view as entryView } from './entry';
 const view = (
   entryViewer: EntryViewer
 ): VNode => {
-  const { filteredEntries, focusedEntryId } = entryViewer;
+  const { filteredEntries, focusedEntryId, selectedEntryId } = entryViewer;
   const entryListItems = filteredEntries.map((entry) => {
     const isFocused = entry.id === focusedEntryId;
+    const isSelected = entry.id === selectedEntryId;
     const className = [
       '.entry-list-item',
       '.entry-id-' + entry.id,
-      isFocused ? '.is-focused' : ''
+      isFocused ? '.is-focused' : '',
+      isSelected ? '.is-selected' : ''
     ].join('');
     return li(className, [entryView(entry)]);
   });
