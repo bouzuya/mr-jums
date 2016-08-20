2e2e6d6911ea5c970fc048686aee2f75210d88de
bouzuya <m@bouzuya.net>
2016-08-20 09:10:55 +0900

    add menu button to view

--- src/view/dom/nav.ts
+++ src/view/dom/nav.ts
@@ -2,6 +2,7 @@ import { VNode, div } from '@cycle/dom';
 
 const view = (): VNode => {
   return div('.nav', [
+    div('.menu', ['H']),
     div('.next', ['J']),
     div('.prev', ['K']),
     div('.enter', ['L'])
