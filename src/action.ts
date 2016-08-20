242abb913be0c5075c78ea722114bbfff86b6b19
bouzuya <m@bouzuya.net>
2016-08-21 08:46:53 +0900

    add FetchPosts*Action

--- src/action.ts
+++ src/action.ts
@@ -1,7 +1,35 @@
-export type ActionType = 'enter' | 'menu' | 'next' | 'prev' | 'select';
+export type ActionType =
+  'enter' |
+  'fetch-posts-failure' |
+  'fetch-posts-request' |
+  'fetch-posts-success' |
+  'menu' |
+  'next' |
+  'prev' |
+  'select';
 
 export interface Action {
   type: ActionType;
+}
+
+export interface FetchPostsFailureAction extends Action {
+  type: 'fetch-posts-failure';
+}
+
+export interface FetchPostsRequestAction extends Action {
+  type: 'fetch-posts-request';
+  request: any; // TODO
+}
+
+export interface FetchPostsSuccessAction extends Action {
+  type: 'fetch-posts-success';
+  posts: {
+    date: string; // yyyy-mm-dd in +09:00
+    minutes: number;
+    pubdate: string; // yyyy-mm-ddThh:mm:ss+09:00
+    tags: string[];
+    title: string;
+  }[];
 }
 
 export interface EnterAction extends Action {
