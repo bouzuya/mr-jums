67e678d981d6c2bc3da3abe811dda36b1d363e0b
bouzuya <m@bouzuya.net>
2016-08-21 10:51:11 +0900

    remove useless comments

--- src/type.ts
+++ src/type.ts
@@ -2,16 +2,8 @@ import { EntryViewer } from './model/entry-viewer';
 
 export { EntryViewer };
 
-// interface EntrySummary {
-//   date: string; // yyyy-mm-dd in +09:00
-//   minutes: number;
-//   pubdate: string; // yyyy-mm-ddThh:mm:ss+09:00
-//   tags: string[];
-//   title: string;
-// }
-
 export interface Entry {
-  id: string; // = date (EntrySummary)
+  id: string;
   title: string;
 }
 
