5d819ea4da80a53c8a383996da51506e4afebdfc
bouzuya <m@bouzuya.net>
2016-08-23 07:24:01 +0900

    add intent for FetchPostSuccessCommand

---
+++ src/intent/fetch-post-success.ts
@@ -0,0 +1,23 @@
+import xs from 'xstream';
+import { Command, FetchPostSuccessCommand } from '../command';
+import { HTTPSource, Response as HTTPResponse } from '@cycle/http';
+
+const intent = ({ HTTP }: { HTTP: HTTPSource; }): xs<Command> => {
+  const response$: xs<HTTPResponse> = HTTP.select('post').flatten();
+  const command$: xs<Command> = response$
+    .map<FetchPostSuccessCommand>((response) => ({
+      type: 'fetch-post-success',
+      post: response.body as { // TODO
+        data: string;
+        date: string;
+        html: string;
+        minutes: number;
+        pubdate: string;
+        tags: string[];
+        title: string;
+      }
+    }));
+  return command$;
+};
+
+export { intent };
--- src/intent/index.ts
+++ src/intent/index.ts
@@ -4,6 +4,7 @@ import { DOMSource } from '@cycle/dom';
 import { HTTPSource } from '@cycle/http';
 
 import { intent as enter$ } from './enter';
+import { intent as fetchPostSuccess$ } from './fetch-post-success';
 import { intent as fetchPostsRequest$ } from './fetch-posts-request';
 import { intent as fetchPostsSuccess$ } from './fetch-posts-success';
 import { intent as menu$ } from './menu';
@@ -14,6 +15,7 @@ import { intent as select$ } from './select';
 const intent = (sources: { DOM: DOMSource; HTTP: HTTPSource }): xs<Command> => {
   const command$: xs<Command> = xs.merge(
     enter$(sources),
+    fetchPostSuccess$(sources),
     fetchPostsRequest$(sources),
     fetchPostsSuccess$(sources),
     menu$(sources),
