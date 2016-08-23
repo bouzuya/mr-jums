96abf0de027711b19870b3a5a2cf56f730408144
bouzuya <m@bouzuya.net>
2016-08-24 08:59:54 +0900

    add simple style for pc, and fix some html

--- src/view/dom/nav.ts
+++ src/view/dom/nav.ts
@@ -1,13 +1,13 @@
-import { VNode, div, del } from '@cycle/dom';
+import { VNode, div } from '@cycle/dom';
 import { State } from '../../type';
 
 const view = ({ menu }: State): VNode => {
   return div('.nav', [
     div('.reload', ['R']),
-    (menu ? del([div(['H'])]) : div('.menu', ['H'])),
+    (menu ? div('.menu-disabled', ['H']) : div('.menu', ['H'])),
     div('.next', ['J']),
     div('.prev', ['K']),
-    (menu ? div('.enter', ['L']) : del([div(['L'])]))
+    (menu ? div('.enter', ['L']) : div('.enter-disabled', ['L']))
   ]);
 };
 
