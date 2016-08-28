65d778d9c38f859aabd94342621171855b2aa31e
bouzuya <m@bouzuya.net>
2016-08-29 08:32:43 +0900

    add HistoryEvent

--- src/event/index.ts
+++ src/event/index.ts
@@ -1,4 +1,5 @@
 export { Event } from './event';
 export { EventType } from './event-type';
+export { HistoryEvent } from './history-event';
 export { RequestEvent } from './request-event';
 export { StateEvent } from './state-event';
