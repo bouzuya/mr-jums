2f34a3801c7a289c5676bb522e19f1dd12942530
bouzuya <m@bouzuya.net>
2016-10-12 07:44:36 +0900

    add view for title-driver

--- src/view/index.ts
+++ src/view/index.ts
@@ -2,6 +2,7 @@ import xs from 'xstream';
 import { view as domView } from './dom';
 import { view as historyView } from './history';
 import { view as httpView } from './http';
+import { view as titleView } from './title';
 import {
   Event,
   EventType,
@@ -20,7 +21,8 @@ const view = (event$: xs<Event>): { DOM: xs<any>; HTTP: xs<any>; } => {
   const sinks = {
     DOM: domView(select<StateEvent>(event$, 'state')),
     HISTORY: historyView(select<HistoryEvent>(event$, 'history')),
-    HTTP: httpView(select<RequestEvent>(event$, 'request'))
+    HTTP: httpView(select<RequestEvent>(event$, 'request')),
+    TITLE: titleView(select<HistoryEvent>(event$, 'history'))
   };
   return sinks;
 };
