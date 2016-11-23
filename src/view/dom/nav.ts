530a0d1093209c505f6d26e8e7e14622379d739b
bouzuya <m@bouzuya.net>
2016-11-23 22:34:37 +0900

    delete reload button

--- src/view/dom/nav.ts
+++ src/view/dom/nav.ts
@@ -3,7 +3,6 @@ import { State } from '../../type';
 
 const view = ({ menu }: State): VNode => {
   return div('.nav', [
-    div('.reload', ['R']),
     (menu ? div('.menu-disabled', ['H']) : div('.menu', ['H'])),
     div('.next', ['J']),
     div('.prev', ['K']),
