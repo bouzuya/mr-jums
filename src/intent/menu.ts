f0e5d2f3363bf3aad4a104cff088c68cfa882673
bouzuya <m@bouzuya.net>
2016-12-02 00:01:14 +0900

    improve H button

--- src/intent/menu.ts
+++ src/intent/menu.ts
@@ -3,7 +3,11 @@ import { Command } from '../command';
 import { DOMSource } from '@cycle/dom';
 
 const intent = ({ DOM }: { DOM: DOMSource }): xs<Command> => {
-  const click$: xs<Event> = DOM.select('div.menu').events('click');
+  const click1$: xs<Event> = DOM.select('div.menu').events('click');
+  const click2$: xs<Event> = DOM.select('.entry-detail')
+    .events('click')
+    .filter((e: Event) => !(<any>e.target).matches('.entry, .entry *'));
+  const click$ = xs.merge(click1$, click2$);
   const command$: xs<Command> = click$.map<Command>(() => ({ type: 'menu' }));
   return command$;
 };
