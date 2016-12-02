ae5a7d962eb4a1c81d851ce17cc365177362ca60
bouzuya <m@bouzuya.net>
2016-12-03 00:40:49 +0900

    fix scrollTop

--- src/view/dom/entry-detail.ts
+++ src/view/dom/entry-detail.ts
@@ -5,7 +5,17 @@ import { view as entryView } from './entry';
 const view = (
   entry: EntryDetail | null
 ): VNode | null => {
-  return div('.entry-detail', [entry === null ? null : entryView(entry)]);
+  return div('.entry-detail', {
+    hook: {
+      postpatch: (_: any, { elm }: VNode): void => {
+        if (typeof elm === 'undefined') return;
+        if (typeof (<any>elm).scrollTop === 'undefined') return;
+        (<any>elm).scrollTop = 0;
+      }
+    }
+  }, [
+      entry === null ? null : entryView(entry)
+    ]);
 };
 
 export { view };
