1a6a10272dc781deedec9baad0f620af450bdf91
bouzuya <m@bouzuya.net>
2016-12-12 19:37:23 +0900

    fix a test

--- test/model/entry-viewer/focus-next.ts
+++ test/model/entry-viewer/focus-next.ts
@@ -16,21 +16,21 @@ test('model > entry-viewer > focusNext', () => {
   assert(next1.selectedEntry === null);
   assert(next1.selectedEntryId === null);
 
-  const next10 = new Array(10).fill(0).reduce((e, _) => e.focusNext(), data);
+  const next10 = new Array(10).fill(0).reduce((e, _) => focusNext(e), data);
   assert(next10.filteredEntries.length === 10);
   assert(next10.filteredEntries[0].id === '2016-01-30');
   assert(next10.focusedEntryId === '2016-01-21');
   assert(next10.selectedEntry === null);
   assert(next10.selectedEntryId === null);
 
-  const next30 = new Array(30).fill(0).reduce((e, _) => e.focusNext(), data);
+  const next30 = new Array(30).fill(0).reduce((e, _) => focusNext(e), data);
   assert(next30.filteredEntries.length === 10);
   assert(next30.filteredEntries[0].id === '2016-01-10');
   assert(next30.focusedEntryId === '2016-01-01');
   assert(next30.selectedEntry === null);
   assert(next30.selectedEntryId === null);
 
-  const next31 = new Array(31).fill(0).reduce((e, _) => e.focusNext(), data);
+  const next31 = new Array(31).fill(0).reduce((e, _) => focusNext(e), data);
   assert(next31.filteredEntries.length === 10);
   assert(next31.filteredEntries[0].id === '2016-01-10');
   assert(next31.focusedEntryId === '2016-01-01');
