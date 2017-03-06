d25a8ba693d41049368f4068ec605c55786b9e4b
bouzuya <m@bouzuya.net>
2017-03-06 21:26:58 +0900

    sort commands

--- src/common/command/index.ts
+++ src/common/command/index.ts
@@ -1,13 +1,18 @@
+// basic
 export { Command } from './command';
 export { CommandType } from './command-type';
+
+// user command
 export { BackCommand } from './back-command';
 export { EnterCommand } from './enter-command';
+export { MenuCommand } from './menu-command';
+export { NextCommand } from './next-command';
+export { PrevCommand } from './prev-command';
+export { SelectCommand } from './select-command';
+
+// internal command
 export { FetchPostSuccessCommand } from './fetch-post-success-command';
 export { FetchPostsFailureCommand } from './fetch-posts-failure-command';
 export { FetchPostsRequestCommand } from './fetch-posts-request-command';
 export { FetchPostsSuccessCommand } from './fetch-posts-success-command';
-export { MenuCommand } from './menu-command';
-export { NextCommand } from './next-command';
 export { NoopCommand } from './noop-command';
-export { PrevCommand } from './prev-command';
-export { SelectCommand } from './select-command';
