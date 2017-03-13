1f5a9cf42b37c4db4e622dfca33d4c523b7e3713
bouzuya <m@bouzuya.net>
2017-03-13 09:01:39 +0900

    remove createImpl()

--- src/common/model/entry-viewer/index.ts
+++ src/common/model/entry-viewer/index.ts
@@ -1,6 +1,5 @@
 import { Entry } from '../../type/entry';
 import { EntryViewer } from '../../type/entry-viewer';
-import { createImpl } from './create-impl';
 import { focusNext } from './focus-next';
 import { focusPrev } from './focus-prev';
 import { getCurrentPageEntries } from './get-current-page-entries';
@@ -9,11 +8,11 @@ import { selectNext } from './select-next';
 import { selectPrev } from './select-prev';
 
 const create = (entries: Entry[]): EntryViewer => {
-  return createImpl(
+  return {
     entries,
-    (entries.length > 0 ? entries[0].id : null),
-    null
-  );
+    focusedEntryId: (entries.length > 0 ? entries[0].id : null),
+    selectedEntryId: null
+  };
 };
 
 export {
