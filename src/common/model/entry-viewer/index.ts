ddf5bf0d28f4374756e45dfb6df4aac56f56ab62
bouzuya <m@bouzuya.net>
2017-03-15 07:53:42 +0900

    prev / next button is disabled on top / bottom

--- src/common/model/entry-viewer/index.ts
+++ src/common/model/entry-viewer/index.ts
@@ -7,6 +7,9 @@ import { select } from './select';
 import { selectNext } from './select-next';
 import { selectPrev } from './select-prev';
 
+import { getNextEntry as getNextEntryImpl } from './get-next-entry';
+import { getPrevEntry as getPrevEntryImpl } from './get-prev-entry';
+
 const create = (entries: Entry[]): EntryViewer => {
   return {
     entries,
@@ -15,11 +18,35 @@ const create = (entries: Entry[]): EntryViewer => {
   };
 };
 
+const getNextFocusedEntry = (entryViewer: EntryViewer): Entry | null => {
+  const { entries, focusedEntryId } = entryViewer;
+  return getNextEntryImpl(entries, focusedEntryId);
+};
+
+const getNextSelectedEntry = (entryViewer: EntryViewer): Entry | null => {
+  const { entries, selectedEntryId } = entryViewer;
+  return getNextEntryImpl(entries, selectedEntryId);
+};
+
+const getPrevFocusedEntry = (entryViewer: EntryViewer): Entry | null => {
+  const { entries, focusedEntryId } = entryViewer;
+  return getPrevEntryImpl(entries, focusedEntryId);
+};
+
+const getPrevSelectedEntry = (entryViewer: EntryViewer): Entry | null => {
+  const { entries, selectedEntryId } = entryViewer;
+  return getPrevEntryImpl(entries, selectedEntryId);
+};
+
 export {
   create,
   focusNext,
   focusPrev,
   getCurrentPageEntries,
+  getNextFocusedEntry,
+  getNextSelectedEntry,
+  getPrevFocusedEntry,
+  getPrevSelectedEntry,
   select,
   selectNext,
   selectPrev
--- src/common/view/nav.ts
+++ src/common/view/nav.ts
@@ -1,15 +1,28 @@
 import { VNode, div } from '@cycle/dom';
 import { State } from '../type/state';
 
-const view = ({ menu }: State): VNode => {
+import {
+  getNextFocusedEntry,
+  getNextSelectedEntry,
+  getPrevFocusedEntry,
+  getPrevSelectedEntry
+} from '../model/entry-viewer';
+
+const view = ({ entryViewer, menu }: State): VNode => {
+  const hasNext = menu === true
+    ? getNextFocusedEntry(entryViewer) !== null
+    : getNextSelectedEntry(entryViewer) !== null;
+  const hasPrev = menu === true
+    ? getPrevFocusedEntry(entryViewer) !== null
+    : getPrevSelectedEntry(entryViewer) !== null;
   const up = '\u25b2'; // U+25B2 BLACK UP-POINTING TRIANGLE
   const down = '\u25bc'; // U+25BC BLACK DOWN-POINTING TRIANGLE
   const right = '\u25b6'; // U+25B6 BLACK RIGHT-POINTING TRIANGLE
   const left = '\u25c0'; // U+25C0 BLACK LEFT-POINTING TRIANGLE
   return div('.nav', { class: { 'is-menu': menu } }, [
     div('.menu', [left]),
-    div('.next', [down]),
-    div('.prev', [up]),
+    div('.next', { class: { 'is-disabled': !hasNext } }, [down]),
+    div('.prev', { class: { 'is-disabled': !hasPrev } }, [up]),
     div('.enter', [right])
   ]);
 };
