60c10e05095408722c60d1e13042324e38e0298a
bouzuya <m@bouzuya.net>
2017-03-19 15:44:59 +0900

    change base url

--- scripts/rev.js
+++ scripts/rev.js
@@ -1,11 +1,12 @@
 var rev = require('rev-hash');
 var fs = require('fs');
 
+var baseUrl = 'https://mr-jums.bouzuya.net';
 var revFile = 'rev.json';
 var jsFile = 'public/scripts/index.js';
 var jsRev = rev(fs.readFileSync(jsFile));
 var newJsFile = 'public/scripts/index-{hash}.js'.replace('{hash}', jsRev);
-var scriptUrl = '/scripts/index-{hash}.js'.replace('{hash}', jsRev);
+var scriptUrl = baseUrl + '/scripts/index-{hash}.js'.replace('{hash}', jsRev);
 var revJson = { scriptUrl };
 
 fs.renameSync(jsFile, newJsFile);
