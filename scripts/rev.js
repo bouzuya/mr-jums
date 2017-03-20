88fb61fc11cf7f70a482414d9109d94c1876fb4c
bouzuya <m@bouzuya.net>
2017-03-20 11:34:49 +0900

    change style url

--- scripts/rev.js
+++ scripts/rev.js
@@ -3,11 +3,20 @@ var fs = require('fs');
 
 var baseUrl = 'https://mr-jums.bouzuya.net';
 var revFile = 'rev.json';
+
 var jsFile = 'public/scripts/index.js';
 var jsRev = rev(fs.readFileSync(jsFile));
 var newJsFile = 'public/scripts/index-{hash}.js'.replace('{hash}', jsRev);
 var scriptUrl = baseUrl + '/scripts/index-{hash}.js'.replace('{hash}', jsRev);
-var revJson = { scriptUrl };
+
+var cssFile = 'public/styles/index.css';
+var cssRev = rev(fs.readFileSync(cssFile));
+var newCssFile = 'public/styles/index-{hash}.css'.replace('{hash}', cssRev);
+var styleUrl = baseUrl + '/styles/index-{hash}.css'.replace('{hash}', cssRev);
+
+var revJson = { scriptUrl, styleUrl };
 
 fs.renameSync(jsFile, newJsFile);
+fs.renameSync(cssFile, newCssFile);
+
 fs.writeFileSync(revFile, JSON.stringify(revJson), { encoding: 'utf-8' });
--- src/common/model/server-config/create.ts
+++ src/common/model/server-config/create.ts
@@ -3,18 +3,20 @@ import { join } from 'path';
 
 import { ServerConfig } from '../../type/server-config';
 
-const getScriptUrl = (): string => {
+const getUrls = (): { scriptUrl: string; styleUrl: string; } => {
   const revJson = JSON.parse(readFileSync('rev.json', { encoding: 'utf-8' }));
-  return revJson.scriptUrl;
+  return revJson;
 };
 
 const create = (): ServerConfig => {
   const imageBaseUrl = process.env.IMAGE_BASE_URL;
   const port = parseInt((process.env.PORT || '4000'), 10);
   const publicDir = join(process.cwd(), 'public');
-  const scriptUrl = (process.env.NODE_ENV === 'development'
-    ? 'http://localhost:3001/scripts/index.js' : getScriptUrl());
-  const styleUrl = '/index.css';
+  const urls = getUrls();
+  const scriptUrl = process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3001/scripts/index.js' : urls.scriptUrl;
+  const styleUrl = process.env.NODE_ENV === 'development'
+    ? 'http://localhost:4000/styles/index.css' : urls.styleUrl;
   return {
     imageBaseUrl,
     port,
