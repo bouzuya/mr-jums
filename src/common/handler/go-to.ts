3243590e152ad0387c173fe0081341c2916f8d67
bouzuya <m@bouzuya.net>
2017-04-02 19:13:17 +0900

    fix go-to handler

--- src/common/handler/go-to.ts
+++ src/common/handler/go-to.ts
@@ -1,13 +1,11 @@
 import xs from 'xstream';
 
-import { NoopCommand } from '../../common/command';
 import { route } from '../../server/route'; // TODO
 import { GoToCommand, MenuCommand, SelectCommand } from '../command';
 import { Message } from '../model/message';
 import { select } from './util/select';
 
 const model = (message$: xs<Message>): xs<Message> => {
-  const noop: NoopCommand = { type: 'noop' };
   return select<GoToCommand>(message$, 'go-to')
     .map(({ path }): SelectCommand | MenuCommand => {
       const { name, params } = route(path);
