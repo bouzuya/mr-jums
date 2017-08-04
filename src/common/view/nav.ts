00b0383ceb9eef391d29a421e760341abaa0380f
bouzuya <m@bouzuya.net>
2017-08-04 21:32:54 +0900

    :art:

--- src/common/view/nav.ts
+++ src/common/view/nav.ts
@@ -1,38 +1,44 @@
 import { VNode, a, div } from '@cycle/dom';
+import { Entry } from '../type/entry';
 import { State } from '../type/state';
 
 import {
+  getCurrentFocusedEntry,
   getNextFocusedEntry,
   getNextSelectedEntry,
   getPrevFocusedEntry,
   getPrevSelectedEntry
 } from '../model/entry-viewer';
 
+const toPath = (entry: Entry | null, type: 'focus' | 'select'): string => {
+  switch (type) {
+    case 'focus':
+      return entry === null ? '/' : '/?f=' + entry.id;
+    case 'select':
+      return entry === null ? '/' : '/' + entry.id.split('-').join('/') + '/';
+    default:
+      throw new Error();
+  }
+};
+
 const view = ({ entryViewer, focus }: State): VNode => {
-  const cf = entryViewer.focusedEntryId;
+  const isMenu = focus === 'entry-list';
+  const cf = getCurrentFocusedEntry(entryViewer);
   const nf = getNextFocusedEntry(entryViewer);
   const ns = getNextSelectedEntry(entryViewer);
   const pf = getPrevFocusedEntry(entryViewer);
   const ps = getPrevSelectedEntry(entryViewer);
-  const hasNext = focus === 'entry-list' ? nf !== null : ns !== null;
-  const hasPrev = focus === 'entry-list' ? pf !== null : ps !== null;
+  const hasNext = isMenu ? nf !== null : ns !== null;
+  const hasPrev = isMenu ? pf !== null : ps !== null;
   const up = '\u25b2'; // U+25B2 BLACK UP-POINTING TRIANGLE
   const down = '\u25bc'; // U+25BC BLACK DOWN-POINTING TRIANGLE
   const right = '\u25b6'; // U+25B6 BLACK RIGHT-POINTING TRIANGLE
   const left = '\u25c0'; // U+25C0 BLACK LEFT-POINTING TRIANGLE
-  const menuHref = focus === 'entry-list'
-    ? '/'
-    : (cf === null ? '/' : '/?f=' + cf.split('-').join('-'));
-  const nextHref = focus === 'entry-list'
-    ? (nf === null ? '/' : '/?f=' + nf.id.split('-').join('-'))
-    : (ns === null ? '/' : '/' + ns.id.split('-').join('/') + '/');
-  const prevHref = focus === 'entry-list'
-    ? (pf === null ? '/' : '/?f=' + pf.id.split('-').join('-'))
-    : (ps === null ? '/' : '/' + ps.id.split('-').join('/') + '/');
-  const enterHref = focus === 'entry-list'
-    ? (cf === null ? '/' : '/' + cf.split('-').join('/') + '/')
-    : '/';
-  return div('.nav', { class: { 'is-menu': focus === 'entry-list' } }, [
+  const menuHref = isMenu ? '/' : toPath(cf, 'focus');
+  const nextHref = isMenu ? toPath(nf, 'focus') : toPath(ns, 'select');
+  const prevHref = isMenu ? toPath(pf, 'focus') : toPath(ps, 'select');
+  const enterHref = isMenu ? toPath(cf, 'select') : '/';
+  return div('.nav', { class: { 'is-menu': isMenu } }, [
     a('.menu', { attrs: { href: menuHref } }, [left]),
     a('.next', {
       class: { 'is-disabled': !hasNext },
