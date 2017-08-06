60532811aa81cdee0fe681c4ffddbd56d91a2850
bouzuya <m@bouzuya.net>
2017-08-06 17:57:48 +0900

    add cli/

---
+++ src/cli/index.ts
@@ -0,0 +1,42 @@
+import { readFileSync } from 'fs';
+import { outputFileSync } from 'fs-extra';
+import { join } from 'path';
+import { parse } from 'url';
+import { create } from '../common/model/server-config/create';
+import { requestHtml } from '../server/request-html';
+
+const build = (dstDir: string) => {
+  const config = create();
+  const jsonDir = parse(config.jsonBaseUrl).path;
+  if (typeof jsonDir === 'undefined') throw new Error();
+  const entriesData = readFileSync(join(jsonDir, 'posts.json'), 'utf-8');
+  const entries = JSON.parse(entriesData) as { date: string; }[];
+  // paths = ['/' + '/2006/01/02/', '/2006/01/02/related/', '/2006/01/03/', ...]
+  const paths = entries
+    .map(({ date }) => '/' + date.split('-').join('/') + '/')
+    .reduce((a, i) => a.concat([i, i + 'related/']), ['/']);
+  paths
+    .reduce((promise, path) => {
+      return promise
+        .then(() => new Promise((resolve) => process.nextTick(resolve)))
+        .then(() => requestHtml(path, config))
+        .then((html) => {
+          const filePaths = [
+            join(dstDir, path, 'index.html')
+          ].concat(path.length === 1 ? [] : [
+            join(dstDir, path.substring(0, path.length - 1) + '.html')
+          ]);
+          filePaths.forEach((p) => {
+            outputFileSync(p, html);
+            console.log(p);
+          });
+        });
+    }, Promise.resolve())
+    .then(() => {
+      console.log('completed');
+    }, (error) => {
+      console.error(error);
+    });
+};
+
+export { build };
