74998e762c48999500e52bc55390d7980dd2476a
bouzuya <m@bouzuya.net>
2017-08-10 09:26:11 +0900

    add ogp & some meta tags

--- src/common/view/html.ts
+++ src/common/view/html.ts
@@ -21,20 +21,35 @@ const view = (
   const faviconUrl = `${imageBaseUrl}/images/favicon.png`;
   const entry = state.selectedEntryDetail;
   const escapedInitialState = htmlescape(serialize(state));
-  return html({ lang: 'ja' }, [
+  const title1 = (entry === null
+    ? '' : entry.id + ' ' + entry.title + ' - ') +
+    'blog.bouzuya.net';
+  const ogTitle = title1;
+  const ogUrl = 'http://blog.bouzuya.net' + (entry === null
+    ? '/'
+    : '/' + entry.id.split('-').join('/') + '/');
+  const ogImageUrl = 'http://blog.bouzuya.net/images/favicon.png';
+  const ogDescription = entry === null ? 'blog.bouzuya.net' : entry.description;
+  const ogSiteName = 'blog.bouzuya.net';
+  return html({ lang: 'ja', prefix: 'og: http://ogp.me/ns#' }, [
     head([
       meta({ props: { charset: 'UTF-8' } }),
-      title([
-        (entry === null
-          ? '' : entry.id + ' ' + entry.title + ' - ') +
-        'blog.bouzuya.net'
-      ]),
+      title([title1]),
+      meta({ props: { name: 'robots', content: 'index, follow' } }),
       meta({
         props: {
           name: 'viewport',
           content: 'width=device-width,initial-scale=1'
         }
       }),
+      meta({ props: { name: 'twitter:card', content: 'summary' } }),
+      meta({ props: { name: 'twitter:site', content: '@bouzuya' } }),
+      meta({ props: { name: 'twitter:creator', content: '@bouzuya' } }),
+      meta({ props: { name: 'og:title', content: ogTitle } }),
+      meta({ props: { name: 'og:url', content: ogUrl } }),
+      meta({ props: { name: 'og:image', content: ogImageUrl } }),
+      meta({ props: { name: 'og:description', content: ogDescription } }),
+      meta({ props: { name: 'og:site_name', content: ogSiteName } }),
       meta({
         props: {
           name: 'theme-color',
@@ -52,6 +67,9 @@ const view = (
       //     rel: 'stylesheet', type: 'text/css', href: styleUrl
       //   }
       // }),
+      entry === null
+        ? link({ props: { rel: 'alternate', type: 'application/atom+xml', href: '/atom.xml' } })
+        : null,
       link({ props: { rel: 'icon', sizes: '192x192', href: faviconUrl } }),
       link({
         props: { rel: 'apple-touch-icon', sizes: '192x192', href: faviconUrl }
