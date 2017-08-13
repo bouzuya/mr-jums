e8ea6360e4ab26325a1ff5d8092644ed0c500994
bouzuya <m@bouzuya.net>
2017-08-13 15:47:15 +0900

    improve nav view html

--- src/common/view/nav.ts
+++ src/common/view/nav.ts
@@ -1,4 +1,4 @@
-import { VNode, a, div } from '@cycle/dom';
+import { VNode, a, div, footer, header, li, nav, ul } from '@cycle/dom';
 import { Entry } from '../type/entry';
 import { State } from '../type/state';
 
@@ -14,7 +14,7 @@ const toPath = (entry: Entry | null, type: 'focus' | 'select'): string => {
   const path = entry === null ? '/' : '/' + entry.id.split('-').join('/') + '/';
   switch (type) {
     case 'focus':
-      return path + 'related/';
+      return entry === null ? '/' : path + 'related/';
     case 'select':
       return path;
     default:
@@ -35,21 +35,33 @@ const view = ({ entryViewer, focus }: State): VNode => {
   const down = '\u25bc'; // U+25BC BLACK DOWN-POINTING TRIANGLE
   const right = '\u25b6'; // U+25B6 BLACK RIGHT-POINTING TRIANGLE
   const left = '\u25c0'; // U+25C0 BLACK LEFT-POINTING TRIANGLE
-  const menuHref = isMenu ? '/' : toPath(cf, 'focus');
+  const menuHref = toPath(cf, 'focus');
   const nextHref = isMenu ? toPath(nf, 'focus') : toPath(ns, 'select');
   const prevHref = isMenu ? toPath(pf, 'focus') : toPath(ps, 'select');
-  const enterHref = isMenu ? toPath(cf, 'select') : '/';
-  return div('.nav', { class: { 'is-menu': isMenu } }, [
-    a('.menu', { attrs: { href: menuHref } }, [left]),
-    a('.next', {
-      class: { 'is-disabled': !hasNext },
-      attrs: { href: nextHref }
-    }, [down]),
-    a('.prev', {
-      class: { 'is-disabled': !hasPrev },
-      attrs: { href: prevHref }
-    }, [up]),
-    a('.enter', { attrs: { href: enterHref } }, [right])
+  const enterHref = toPath(cf, 'select');
+  return nav('.nav', { class: { 'is-menu': isMenu } }, [
+    header('.header', []),
+    div('.body', [
+      ul('.link-list', [
+        li('.link', [a('.menu', {
+          class: { 'is-disabled': isMenu },
+          attrs: { href: menuHref }
+        }, [left])]),
+        li('.link', [a('.next', {
+          class: { 'is-disabled': !hasNext },
+          attrs: { href: nextHref }
+        }, [down])]),
+        li('.link', [a('.prev', {
+          class: { 'is-disabled': !hasPrev },
+          attrs: { href: prevHref }
+        }, [up])]),
+        li('.link', [a('.enter', {
+          class: { 'is-disabled': !isMenu },
+          attrs: { href: enterHref }
+        }, [right])])
+      ])
+    ]),
+    footer('.footer', [])
   ]);
 };
 
